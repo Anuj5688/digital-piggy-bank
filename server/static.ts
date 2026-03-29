f9b0138b97dcbbb5b93cbebb1a8543ce198be31d
Anuj5688 <kanu08288@gmail.com>
2026-03-29 11:52:49 +0530

    correct public path

--- server/static.ts
+++ server/static.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "../../dist");
+  const distPath = path.resolve(__dirname, "../../dist/public");
 
   console.log("Serving from:", distPath);
 
