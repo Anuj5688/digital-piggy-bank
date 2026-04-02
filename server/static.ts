c3ed8bd4c647fb7d0ac3c747295f1c24a033f795
Anuj5688 <kanu08288@gmail.com>
2026-04-02 12:46:48 +0530

    path fix

--- server/static.ts
+++ server/static.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 export function serveStatic(app: express.Express) {
-  const distPath = path.join(process.cwd(), "../client/dist");
+  const distPath = path.join(process.cwd(), "../client/dist/public");
 
   console.log("Serving from:", distPath);
 
